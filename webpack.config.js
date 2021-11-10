02a9e9029107b5f97e285667f37ad046bd9c9e4a
魏琼 <516948708@qq.com>
2021-11-10 11:30:58 +0800

    feat: 管理资源

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,63 @@
-const path = require('path')
-
+const { resolve } = require('path')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const toml = require('toml')
+const yaml = require('yamljs')
+const json5 = require('json5')
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+      // 将任何 toml、yaml 或 json5 文件作为 JSON 模块导入
+      {
+        test: /\.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse,
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
   },
 }
