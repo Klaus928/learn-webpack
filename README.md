e70b5d5dce321e958b561422713e76ea08ed7285
魏琼 <516948708@qq.com>
2021-11-10 11:51:44 +0800

    feat: 管理输出

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # webpack 指南过程
+
+## 管理资源
+
+除了引入 JavaScript，webpack 还可以通过 loader 或内置的 <a href="https://webpack.docschina.org/guides/asset-modules/">Asset Modules </a>引入任何其他类型的文件
+
+## 管理输出
+
+使用 <a href="https://webpack.docschina.org/plugins/html-webpack-plugin/">html-webpack-plugin</a> 可以动态生成 html 文件并引入打包后的文件， 我们不再需要在 dist 里的 index.html 手动引入
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,17 @@
 const { resolve } = require('path')
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const toml = require('toml')
-const yaml = require('yamljs')
-const json5 = require('json5')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: { index: './src/index.js', print: './src/print.js' },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: resolve(__dirname, 'dist'),
+    clean: true,
   },
-  module: {
-    rules: [
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      // 将任何 toml、yaml 或 json5 文件作为 JSON 模块导入
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-      // `...`,
-      new CssMinimizerPlugin(),
-    ],
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    }),
+  ],
 }
